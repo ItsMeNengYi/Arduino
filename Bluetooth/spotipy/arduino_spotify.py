d11bfe3fe01fab6fcb952d0f297dd8ab313f807f
ItsMeFaquu <87684677+ItsMeFaquu@users.noreply.github.com>
2023-05-04 21:58:11 +0800

    .

--- Bluetooth/spotipy/arduino_spotify.py
+++ Bluetooth/spotipy/arduino_spotify.py
@@ -8,18 +8,13 @@ blue = BluetoothBot()
 
 current_playing = spot.get_current_track()
 blue.send_current_track(current_playing)
-start_time = time.time()
-while (True):
-    current_time = time.time() 
-    elapsed_time = current_time - start_time
-    # if(input()):
-    #     print("END")
-    #     blue.close()
-    #     break
-    # spot.run_command(blue.get_command())
-    if(current_playing!=spot.get_current_track()):
-        current_playing = spot.get_current_track()
-        blue.send_current_track(current_playing)
+try:
+    while (True):
+        spot.run_command(blue.get_command())
+        if(current_playing!=spot.get_current_track()):
+            current_playing = spot.get_current_track()
+            blue.send_current_track(current_playing)
 
-    time.sleep(1)
-
+        time.sleep(1)
+except ERROR:
+    blue.close()
